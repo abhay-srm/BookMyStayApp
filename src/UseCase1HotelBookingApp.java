f967f4c50c62067a2000b74febfd669f4128cc2c
Abhay Aggarwal <aa1716@srmist.edu.in>
2026-03-11 15:20:53 +0530

    Initial commit - Use Case 1 Application Entry

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,26 @@
+/**
+
+ * Book My Stay Application
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It demonstrates how a Java program begins execution using the main() method
+ * and prints a welcome message to the console.
+ *
+ * @author Admin
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+
+     * Main method – Entry point of the application
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+    }
+}
