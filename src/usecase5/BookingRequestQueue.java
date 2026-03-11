b579d525d36bc93c6fd10c978d09b5165e64cb9b
Abhay Aggarwal <aa1716@srmist.edu.in>
2026-03-11 15:47:17 +0530

    Implemented Use Case 5 – Booking Request Queue (FIFO)

---
+++ src/usecase5/BookingRequestQueue.java
@@ -0,0 +1,31 @@
+package usecase5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Manages incoming booking requests using a FIFO queue.
+ */
+public class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    // Add booking request
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+        System.out.println("Booking request added for " + reservation.getGuestName());
+    }
+
+    // View queue
+    public void displayQueue() {
+        System.out.println("\nCurrent Booking Requests in Queue:\n");
+
+        for (Reservation r : requestQueue) {
+            r.displayReservation();
+        }
+    }
+}
